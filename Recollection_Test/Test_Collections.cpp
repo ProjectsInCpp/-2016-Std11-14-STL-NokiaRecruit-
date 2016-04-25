e51d7b0a8c887cad2dd18f68e43015fe053372b8
korytkoBT <209826@student.pwr.wroc.pl>
2016-04-25 20:18:04 +0000

    T1: unordered_map; T2: Access to pointer after find by ->; T3: Multi_map

--- Recollection_Test/Test_Collections.cpp
+++ Recollection_Test/Test_Collections.cpp
@@ -4,6 +4,7 @@
 #include "Utils.h"
 #include <vector>
 #include <map>
+#include <unordered_map>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
@@ -239,13 +240,13 @@ namespace Recollection_Test
 			};
 			int searchedKey = 5;
 
-			vint expTape = { 1 ,2 ,3 ,4 ,5, 6 };
 			vector<string> rcVal;
 			vector<string> expVal{ "piec"};
 			int i = 0;
 			bool endVal{ true };
 
 			//act
+			inTape.insert({5,"five"});
 			auto elem = inTape.find(searchedKey);
 			while (elem != inTape.end())
 				rcVal.push_back((*elem++).second);
@@ -256,5 +257,160 @@ namespace Recollection_Test
 				
 			Assert::IsTrue(endVal);
 		}
+
+		TEST_METHOD(Map_Int_String_Find_1_True)
+		{
+			//arrange
+			map<int, string> inTape
+			{
+				{ 1,"jeden" },
+				{ 2,"dwa" },
+				{ 3,"trzy" },
+				{ 4,"cztery" },
+				{ 5,"piec" }
+			};
+			int searchedKey = 1;
+			string expVal{ "jeden" };
+			string rcVal{ "" };
+
+			//act
+			rcVal = inTape.find(searchedKey)->second;
+
+			//assert
+			Assert::AreEqual(expVal, rcVal);
+		}
+
+		TEST_METHOD(Map_Int_String_InsertDuplicate_Size_NotChanged)
+		{
+			//arrange
+			map<int, string> inTape
+			{
+				{ 1,"jeden" },
+				{ 2,"dwa" },
+				{ 3,"trzy" },
+				{ 4,"cztery" },
+				{ 5,"piec" }
+			};
+			int expSize = inTape.size();
+			int rcSize = 0;
+			
+			//act
+			inTape.insert({ 5,"five" });
+			rcSize = inTape.size();
+
+			//assert
+
+			Assert::AreEqual(expSize, rcSize);
+		}
+		TEST_METHOD(MultiMap_Int_String_InsertDuplicate_Size_Changed)
+		{
+			//arrange
+			multimap<int, string> inTape
+			{
+				{ 1,"jeden" },
+				{ 2,"dwa" },
+				{ 3,"trzy" },
+				{ 4,"cztery" },
+				{ 5,"piec" }
+			};
+			int expSize = inTape.size()+1;
+			int rcSize = 0;
+
+			//act
+			inTape.insert({ 5,"five" });
+			rcSize = inTape.size();
+
+			//assert
+
+			Assert::AreEqual(expSize, rcSize);
+		}
+
+		TEST_METHOD(MultiMap_Int_String_AccessToValue)
+		{
+			//arrange
+			multimap<int, string> inTape
+			{
+				{ 1,"jeden" },
+				{ 2,"dwa" },
+				{ 3,"trzy" },
+				{ 4,"cztery" },
+				{ 5,"piec" }
+			};
+			int expSize = inTape.size() + 1;
+			int searchedKey = 5;
+			int rcSize = 0;
+			bool expVal = true;
+			vector<string> rcTape{ };
+			vector<string> expTape{ "piec", "five" };
+			int i = 0;
+
+			//act
+			inTape.insert({ 5,"five" });
+			auto it = inTape.find(searchedKey);
+			while (it != inTape.cend())
+				rcTape.push_back((*it++).second);
+
+			for (auto const val : rcTape)
+				expVal &= val == expTape[i++];
+
+			//assert
+			Assert::IsTrue(expVal);
+		}
+
+		TEST_METHOD(UnorderedMap_Int_String_InsertDuplicate_Size_Changed)
+		{
+			//arrange
+			unordered_map<int, string> inTape
+			{
+				{ 1,"jeden" },
+				{ 2,"dwa" },
+				{ 3,"trzy" },
+				{ 4,"cztery" },
+				{ 5,"piec" }
+			};
+			int expSize = inTape.size();
+			int rcSize = 0;
+
+			//act
+			inTape.insert({ 5,"five" });
+			rcSize = inTape.size();
+
+			//assert
+
+			Assert::AreEqual(expSize, rcSize);
+		}
+
+		TEST_METHOD(UnorderedMap_Int_String_AccessToValue)
+		{
+			//arrange
+			unordered_map<int, string> inTape
+			{
+				{ 1,"jeden" },
+				{ 2,"dwa" },
+				{ 3,"trzy" },
+				{ 4,"cztery" },
+				{ 5,"piec" }
+			};
+			int expSize = inTape.size() + 1;
+			int searchedKey = 5;
+			int rcSize = 0;
+			bool expVal = true;
+			vector<string> rcTape{};
+			vector<string> expTape{ "piec", "five" };
+			int i = 0;
+
+			//act
+			inTape.insert({ 5,"five" });
+			auto it = inTape.find(searchedKey);
+			while (it != inTape.cend())
+				rcTape.push_back((it++)->second);
+
+			for (auto const val : rcTape)
+				expVal &= val == expTape[i++];
+
+			//assert
+			Assert::IsTrue(expVal);
+		}
+
 	};
 }
