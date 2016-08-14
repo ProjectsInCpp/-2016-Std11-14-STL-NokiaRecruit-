308a551c201797340268f98959cd1a9295d7e8c8
korytkoBT <209826@student.pwr.wroc.pl>
2016-08-14 19:11:27 +0000

    Auto conversions in Cpp'

---
+++ Recollection_Console/Conversions.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+
+class SimplePackage
+{
+public:
+    inline static std::string retVal(int arg)
+    {
+        return std::to_string(arg);
+    }
+};
+
+int main()
+{
+    std::cout << SimplePackage::retVal(true) << "\n";
+    std::cout << SimplePackage::retVal('A') << "\n";
+    std::cout << SimplePackage::retVal(5.5) << "\n";
+    std::cout << SimplePackage::retVal(5) << "\n";
+    std::cout << SimplePackage::retVal(0x0100) << "\n";
+    // if you will hover over hex rValue, then it will show you int representation
+    system("pause");
+}
+
--- Recollection_Console/ScopedEnums.cpp
+++ Recollection_Console/ScopedEnums.cpp
@@ -1,17 +1,17 @@
 #include "stdafx.h"
 #include <iostream>
 
-int main()
-{
-    /* The code below will not compile because old enums 
-    ** don't allow to use "names" declared in it's scope
-    ** In scope leveled upper
-    enum ENUM { one, two};
-    int one = 1;
-    */
-
-    enum class ENUM {one, two};
-    int one = 1;
-
-    system("pause");
-}
+//int main()
+//{
+//    /* The code below will not compile because old enums 
+//    ** don't allow to use "names" declared in it's scope
+//    ** In scope leveled upper
+//    enum ENUM { one, two};
+//    int one = 1;
+//    */
+//
+//    enum class ENUM {one, two};
+//    int one = 1;
+//
+//    system("pause");
+//}
