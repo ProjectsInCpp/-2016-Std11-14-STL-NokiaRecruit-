e458ae3391bb104f396aff761c5770ea480216ba
korytkoBT <209826@student.pwr.wroc.pl>
2016-08-14 19:38:08 +0000

    OwnStreamManipulator
    Now dont work, mind about specialization templates for choosen types'

--- Recollection_Console/Conversions.cpp
+++ Recollection_Console/Conversions.cpp
@@ -10,15 +10,15 @@ public:
         return std::to_string(arg);
     }
 };
-
-int main()
-{
-    std::cout << SimplePackage::retVal(true) << "\n";
-    std::cout << SimplePackage::retVal('A') << "\n";
-    std::cout << SimplePackage::retVal(5.5) << "\n";
-    std::cout << SimplePackage::retVal(5) << "\n";
-    std::cout << SimplePackage::retVal(0x0100) << "\n";
-    // if you will hover over hex rValue, then it will show you int representation
-    system("pause");
-}
+//
+//int main()
+//{
+//    std::cout << SimplePackage::retVal(true) << "\n";
+//    std::cout << SimplePackage::retVal('A') << "\n";
+//    std::cout << SimplePackage::retVal(5.5) << "\n";
+//    std::cout << SimplePackage::retVal(5) << "\n";
+//    std::cout << SimplePackage::retVal(0x0100) << "\n";
+//    // if you will hover over hex rValue, then it will show you int representation
+//    system("pause");
+//}
 
---
+++ Recollection_Console/OwnStreamManipulator.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+static int allocatedIdx = std::ios_base::xalloc();
+
+std::ios_base& setSeparatorSpace(std::ios_base& inStream)
+{
+    inStream.pword(allocatedIdx) = reinterpret_cast<void*>('a');
+    return inStream;
+}
+
+//std::ostream& operator<<(std::ostream& inStream, int inArg)
+//{
+//    inStream.pword(allocatedIdx) = reinterpret_cast<void*>('a');
+//    inStream.put(reinterpret_cast<char>(inStream.pword(allocatedIdx)));
+//    inStream.put(inArg);
+//    return inStream;
+//}
+
+int main()
+{
+    std::stringstream someStream;
+    std::string someString = "lValueStr";
+    someStream << setSeparatorSpace << someString << "rValueStr" << 3 << 'elo';
+    std::cout << 3;
+}
